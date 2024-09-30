f46782d35b0a86b2bf3583011a81a66b9e574266
Maarifa-C <maarifa.hayat@gmail.com>
2024-09-30 10:47:26 -0400

    update

--- popup.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener((request) => {
-    if (request.message === "send_prompt_button_clicked") {
-        document.getElementById('status').innerText = 'Send button clicked!';
-    }
-});
--- scripts.js
+++
@@ -1,8 +0,0 @@
-function log_click(click_target) {
-    return(click_target);
-}
-
-// normal event listener
-document.addEventListener("click!", (event) => {
-    log_click(event.target);
-});
